d4e72dbfe11e198fda4a9b9a83c9b7ba2940c59f
strayder90 <strayder@live.com>
2022-02-04 18:19:30 +0100

    Moves init of database and table to config.js

---
+++ backend/config.js
@@ -0,0 +1,132 @@
+import mysql from 'mysql2';
+
+export const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'test',
+	database: 'sport_calendar',
+});
+
+const createDatabase = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    } else {
+      connection.query('CREATE DATABASE sport_calendar', error => {
+        if (error) {
+          throw error;
+        }
+        console.log('Database created');
+      });
+    }
+	});
+};
+
+const createSportTable = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'CREATE TABLE `sport` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(45) NULL, PRIMARY KEY (`id`));',
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Table sport created!');
+			}
+		);
+	});
+};
+
+const insertIntoSportTable = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'INSERT INTO sport (id, name) VALUES (?, ?)', [null, 'Football'],
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Inserted into sport table.');
+			}
+		);
+	});
+}
+
+const deleteSportTableData = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'TRUNCATE sport',
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Deleted from sport table.');
+			}
+		);
+	});
+}
+
+const createTeamTable = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'CREATE TABLE `team` (`id` INT NOT NULL AUTO_INCREMENT, `team` VARCHAR(45) NULL, `country` VARCHAR(45) NULL, `acronym` VARCHAR(45) NULL, PRIMARY KEY (`id`));',
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Table team created!');
+			}
+		);
+	});
+};
+
+const insertIntoTeamTable = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'INSERT INTO team (id, team, country, acronym) VALUES (?, ?, ?, ?)', [null, 'Real Madrid', 'Spain', 'RMD'],
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Inserted into team table.');
+			}
+		);
+	});
+}
+
+const deleteTeamTableData = () => {
+	connection.connect(error => {
+		if (error) {
+      throw error;
+    }
+		connection.query(
+			'TRUNCATE team',
+			error => {
+				if (error) {
+          throw error;
+        }
+				console.log('Deleted from team table.');
+			}
+		);
+	});
+}
+
+console.log('Initializing database...')
+createDatabase();
+createSportTable();
+insertIntoSportTable();
+createTeamTable();
+insertIntoTeamTable();
